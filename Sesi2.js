d4b036c248f848b60b29bbaa4b7827b677a5f05a
MuhammadSheva25 <114803984+MuhammadSheva25@users.noreply.github.com>
2023-10-02 16:47:41 +0700

    Add files via upload

---
+++ Sesi2.js
@@ -0,0 +1,9 @@
+var firstName = "Muhammad Sheva";
+var lastName = "Adyaksa Rohendi";
+var age = 20; 
+var isMarried = false; 
+
+console.log("Nama Depan: " + firstName);
+console.log("Nama Belakang: " + lastName);
+console.log("Umur: " + age);
+console.log("Status Pernikahan: " + (isMarried ? "Menikah" : "Belum Menikah"));
